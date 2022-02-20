b52a82de1e75c9b63db5d8d8b57b4620677d9b04
JacobArthurs <Arthursjacob01@gmail.com>
2022-02-20 03:48:44 -0600

    Visual updates to parallax

--- src/components/SkillsSection/Skills.js
+++ src/components/SkillsSection/Skills.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
 import SwiperCore, { Autoplay, FreeMode } from "swiper";
 import "swiper/swiper-bundle.css";
