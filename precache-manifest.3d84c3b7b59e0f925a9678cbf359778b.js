4e0a63789b5a777969424bed907f6d5d13588a56
JacobArthurs <Arthursjacob01@gmail.com>
2022-03-20 11:40:19 -0500

    Updates

---
+++ precache-manifest.3d84c3b7b59e0f925a9678cbf359778b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b265b9f42d99a260592dbeb7e96874a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d94153d8c4d56f641f9d",
+    "url": "/static/css/2.1a9828a8.chunk.css"
+  },
+  {
+    "revision": "f3d136fe68341c25e617",
+    "url": "/static/css/main.027a64b8.chunk.css"
+  },
+  {
+    "revision": "d94153d8c4d56f641f9d",
+    "url": "/static/js/2.85a8f8c0.chunk.js"
+  },
+  {
+    "revision": "5bc1ad34d21a4712076fda8aff156f45",
+    "url": "/static/js/2.85a8f8c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3d136fe68341c25e617",
+    "url": "/static/js/main.0eee11fb.chunk.js"
+  },
+  {
+    "revision": "81dbcad72ece63b3509b",
+    "url": "/static/js/runtime-main.0451c3ff.js"
+  },
+  {
+    "revision": "db7d4687627299c5f4f88f4f99d60a93",
+    "url": "/static/media/JacobArthurs.db7d4687.jpg"
+  },
+  {
+    "revision": "37cd3153b6057982a6bf850a72fc104e",
+    "url": "/static/media/JacobArthursCard.37cd3153.png"
+  },
+  {
+    "revision": "94d6c461bc9a0f884cd49b8f27adb83e",
+    "url": "/static/media/JacobArthursCardBack.94d6c461.png"
+  },
+  {
+    "revision": "68795bab63cd0d44124336d3a961fdc2",
+    "url": "/static/media/JacobArthursNav.68795bab.png"
+  },
+  {
+    "revision": "44b6950ef3046d2dd6d009bddb030407",
+    "url": "/static/media/JacobArthurs_Resume.44b6950e.pdf"
+  },
+  {
+    "revision": "358b92bcaaf77a97f3d0dc43bfbb2533",
+    "url": "/static/media/ParallaxWaves.358b92bc.png"
+  },
+  {
+    "revision": "3740c6e534d6e9be81301dba46bab924",
+    "url": "/static/media/aspnet.3740c6e5.png"
+  },
+  {
+    "revision": "0afd5775295b8428bd9feae263c1e457",
+    "url": "/static/media/csharp.0afd5775.png"
+  },
+  {
+    "revision": "022bba869736d1ef2e5091f33584f646",
+    "url": "/static/media/css.022bba86.png"
+  },
+  {
+    "revision": "fca0f1e305cbd6c333052ec34ea5d9d8",
+    "url": "/static/media/git.fca0f1e3.png"
+  },
+  {
+    "revision": "dc39d4f42f5744a1f96fa1c119fceeed",
+    "url": "/static/media/html.dc39d4f4.png"
+  },
+  {
+    "revision": "ba9b8c0b5467b3294f122c278031f019",
+    "url": "/static/media/js.ba9b8c0b.png"
+  },
+  {
+    "revision": "95decaf33ca35827a5a124d802fdb44b",
+    "url": "/static/media/msSQL.95decaf3.png"
+  },
+  {
+    "revision": "f308bb7c1e8edbc28ab4b1512e38e6a1",
+    "url": "/static/media/react.f308bb7c.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-app-JacobArthurs/precache-manifest.bbbcd9cee95a6cf71d2b7241ef76e5a1.js"
+  "/precache-manifest.3d84c3b7b59e0f925a9678cbf359778b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-app-JacobArthurs/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
